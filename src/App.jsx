69806699e3085769c7f4ca0e93c4e9745b87b253
Rahul <thecoderrahul@gmail.com>
2023-11-05 02:00:47 +0530

    C2

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
+      <div className="section_0">
+          <div className="section_4_text" ><p><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#012E57" d="m10 18l-6-6l6-6l1.4 1.45L7.85 11H20v2H7.85l3.55 3.55L10 18Z"/></svg>View Patient</p> </div>
+        </div>
         <div className="section-1">
           <div>
             <h1>S.Meena, F/23</h1>
